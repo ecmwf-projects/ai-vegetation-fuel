7095fe7960d2abe6106666337a67c0a998944d5c
lazyoracle <contact@anuragsaharoy.me>
2021-03-25 02:39:14 +0530

    utility modules

---
+++ src/utils/fuelload.py
@@ -0,0 +1,37 @@
+import xarray
+import numpy as np
+
+BA_THRESH = 7.7 * 1e6  # Threshold for burned area
+AGB_THRESH = 0  # Threshold for AGB dataset
+
+
+def fuelload(path_agb: str, path_ba: str, path_fl: str = None) -> xarray.Dataset:
+    """This function generates Fuel Load xarray dataset
+
+    Parameters
+    -----------
+    path_agb : str
+        path of AGB dataset
+    path_ba : str
+        path of BA  dataset
+    path_fl : str
+        path to save final fuel load dataset. Defaults to None.
+
+    Returns
+    --------
+    xarray.Dataset
+        Returns xarray dataset of fuel load
+    """
+    da_agb = xarray.open_dataset(path_agb)
+    da_ba = xarray.open_dataset(path_ba)
+    agb_data = da_agb["abg_avitabile_vodmean"][:, :, :]
+    ba_data = da_ba["burned_area"][:, :, :]
+    agb_data.values[agb_data.values == AGB_THRESH] = np.nan
+    ba_data.values[ba_data.values < BA_THRESH] = np.nan
+
+    fuel_load_dataset = agb_data * ba_data
+
+    if path_fl is not None:
+        fuel_load_dataset.to_netcdf(path_fl)
+
+    return fuel_load_dataset
---
+++ src/utils/generate_map_plots.py
@@ -0,0 +1,96 @@
+"""Inference Plotting Module
+
+This module is used to create (png) map projection plots from ML model inference (csv) files.
+
+Note
+-----
+The :code:`.csv` files should be named as :code:`{type}_output_{month}.csv`, where :code:`type` is :code:`tropics` or :code:`midlats`.
+The csv column headers are expected to be :code:`lat | lon | actual_load | predicted_load`.
+"""
+
+
+import pandas as pd
+import geopandas as gpd
+import plotly.express as px
+from shapely.geometry import Point
+
+
+def read_csv(csv_file_path: str) -> pd.DataFrame:
+    """Reads csv with headers, and creates a Pandas dataframe."""
+    df = pd.read_csv(csv_file_path)
+    return df
+
+
+def create_geometry(df: pd.DataFrame) -> pd.DataFrame:
+    """Creates a new geometry column based on the latitude & longitude positions, and saves the Pandas dataframe."""
+    # makes geometry column
+    points = df.apply(
+        lambda row: Point(row.lat, row.lon), axis=1
+    )  # assumes 'lat' and 'lon' as csv column headers for Latitude and Longitude
+    graph = gpd.GeoDataFrame(df, geometry=points)
+    return df, graph
+
+
+def generate_plots(
+    df_with_geometry: pd.DataFrame,
+    output_file_path_act: str,
+    output_file_path_pred: str,
+    month: str,
+) -> None:
+    """Creates a .png of actual & predicted map projection plots."""
+
+    def generate_act_plot(df_with_geometry):
+        """Plots actual Fuel Load."""
+        # assumes 'lat', 'lon' and 'actual_load' as column headers for Latitude, Longitude, and Actual Fuel Load
+        fig_act = px.density_mapbox(
+            df_with_geometry,
+            lat="lat",
+            lon="lon",
+            z="actual_load",
+            radius=3,
+            center=dict(lat=0, lon=180),
+            zoom=0,
+            range_color=[0, 1.592246e11],
+            mapbox_style="open-street-map",
+            title="Actual Fuel Load Estimate for " + month + " 2016",
+        )
+
+        img_bytes_act = fig_act.to_image(format="png", engine="kaleido")
+
+        f_act = open(output_file_path_act, "wb")
+        f_act.write(img_bytes_act)
+        f_act.close()
+        print(
+            "Actual FL plot successfully generated! File saved to ",
+            output_file_path_act,
+        )
+
+    def generate_pred_plot(df_with_geometry):
+        """Plots predicted Fuel Load."""
+        # assumes 'lat', 'lon' and 'predicted_load' as column headers for Latitude, Longitude, and Predicted Fuel Load
+        fig_pred = px.density_mapbox(
+            df_with_geometry,
+            lat="lat",
+            lon="lon",
+            z="predicted_load",
+            radius=3,
+            center=dict(lat=0, lon=180),
+            zoom=0,
+            range_color=[0, 1.592246e11],
+            mapbox_style="open-street-map",
+            title="Predicted Fuel Load Estimate for " + month + " 2016",
+        )
+
+        img_bytes_pred = fig_pred.to_image(format="png", engine="kaleido")
+
+        f_pred = open(output_file_path_pred, "wb")
+        f_pred.write(img_bytes_pred)
+        f_pred.close()
+        print(
+            "Predicted FL plot successfully generated! File saved to ",
+            output_file_path_pred,
+        )
+
+    if "actual_load" in df_with_geometry.columns:
+        generate_act_plot(df_with_geometry)
+    generate_pred_plot(df_with_geometry)
